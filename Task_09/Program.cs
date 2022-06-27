c3705cab22e7a648ef1b0fb591e9b45d77e910b0
Diana <diana@example.com>
2022-06-27 22:39:15 +0300

    Решены все задачи с семинара 2

---
+++ Task_09/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая выводит
+случайное число из отрезка [10, 99] и показывает
+наибольшую цифру числа.
+78 -> 8
+12-> 2
+85 -> 8 */
+
+
+int randNum = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число {randNum}");
+
+int dig1 = randNum % 10;
+int dig2 = randNum / 10;
+digMax = dig1 > dig2 ? dig1 : dig2;
+
+//if (dig1 > dig2) Console.WriteLine($"Наибольшая цифра числа {randNum} => {dig1}");
+//else if (dig2 > dig1) ($"Наибольшая цифра числа {randNum} => {dig2}");
+//else ($"Цифры числа одинаковы {randNum} => {dig1}");
+
+if (dig1 == dig2) ($"Цифры числа {randNum} одинаковы");
+else ($"Наибольшая цифра числа {randNum} => {digMax}");
---
+++ Task_11/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая выводит случайное
+трёхзначное число и удаляет вторую цифру этого
+числа.
+456 -> 46
+782 -> 72
+918 -> 98 */
+
+int rndNum = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100 - 999 => {rndNum}");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 100;
+    int theDigit = num % 10;
+    return firstDigit * 10 + theDigit;
+}
+Console.WriteLine($"Число нужное вам = {MaxDigit(rndNum)}");
