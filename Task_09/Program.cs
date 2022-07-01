ccfc8207f9958da2e763b58a0055ac8ab673a79e
Diana <diana@example.com>
2022-07-01 16:36:23 +0300

    Исправлена ошибка

--- Task_09/Program.cs
+++ Task_09/Program.cs
@@ -11,11 +11,11 @@ Console.WriteLine($"Случайное число {randNum}");
 
 int dig1 = randNum % 10;
 int dig2 = randNum / 10;
-digMax = dig1 > dig2 ? dig1 : dig2;
+int digMax = dig1 > dig2 ? dig1 : dig2;
 
 //if (dig1 > dig2) Console.WriteLine($"Наибольшая цифра числа {randNum} => {dig1}");
 //else if (dig2 > dig1) ($"Наибольшая цифра числа {randNum} => {dig2}");
 //else ($"Цифры числа одинаковы {randNum} => {dig1}");
 
-if (dig1 == dig2) ($"Цифры числа {randNum} одинаковы");
-else ($"Наибольшая цифра числа {randNum} => {digMax}");
+if (dig1 == dig2) Console.WriteLine($"Цифры числа {randNum} одинаковы");
+else Console.WriteLine($"Наибольшая цифра числа {randNum} => {digMax}");
