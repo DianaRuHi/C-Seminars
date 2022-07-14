2219d495bd66cb324376a883f459ce0d03fec452
Diana <diana@example.com>
2022-07-14 22:03:05 +0300

    Семинар 7

---
+++ Task_49/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Задайте двумерный массив. Найдите элементы, у
+которых оба индекса чётные, и замените эти элементы на их
+квадраты. */
+
+int[,] CreateMatrixRndInt(int m, int n, int min, int max)
+{
+    int[,] arr = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write($"[{arr[i, j],3} |");
+            else if (j < arr.GetLength(1) - 1) Console.Write($"{arr[i, j],3} |");
+            else Console.WriteLine($"{arr[i, j],3} ]");
+        }
+    }
+}
+
+void MatrixEvenIndexPow(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i+=2)
+    {
+        for (int j = 0; j < matr.GetLength(1); j+=2)
+        {
+            matr[i, j] = matr[i,j]*matr[i,j];
+        }
+    }
+}
+
+int[,] matrix = CreateMatrixRndInt(3, 4, 2, 9);
+PrintMatrix(matrix);
+Console.WriteLine();
+MatrixEvenIndexPow(matrix);
+PrintMatrix(matrix);
---
+++ Task_51/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Задайте двумерный массив. Найдите сумму
+элементов, находящихся на главной диагонали (с индексами
+(0,0); (1;1) и т.д. */
+
+int[,] CreateMatrixRndInt(int m, int n, int min, int max)
+{
+    int[,] matr = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write($"[{matr[i, j],3} |");
+            else if (j < matr.GetLength(1) - 1) Console.Write($"{matr[i, j],3} |");
+            else Console.WriteLine($"{matr[i, j],3} ]");
+        }
+    }
+}
+
+int SumMainDiagnal(int[,] matr)
+{
+    int result = 0;
+    int minIndex = (matr.GetLength(0) < matr.GetLength(1)) ? matr.GetLength(0) : matr.GetLength(1);
+    for (int i = 0; i < minIndex; i++)
+    {
+        result += matr[i, i];
+    }
+    return result;
+}
+
+int[,] matrix = CreateMatrixRndInt(6, 3, 0, 10);
+PrintMatrix(matrix);
+int sumDiagnal = SumMainDiagnal(matrix);
+Console.WriteLine($"Сумма элементов по диагонали равна {sumDiagnal}");
