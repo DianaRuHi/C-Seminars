77eb2ce8f723ea7514b1f32ea0e07f17e7beff34
Diana <diana@example.com>
2022-07-22 02:53:29 +0300

    Семинар 9

---
+++ Task_63/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задайте значение N. Напишите программу, которая выведет
+все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6" */
+
+Console.WriteLine("Введите число:");
+Console.Write ("N = ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Numbers1N(int n)
+{
+    if (n==0) return;
+    Numbers1N (n-1);
+    Console.Write ($"{n} ");
+}
+
+Numbers1N(num);
---
+++ Task_67/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая будет принимать на вход число и
+возвращать сумму его цифр.
+453 -> 12
+45 -> 9 */
+
+Console.WriteLine("Введите число:");
+Console.Write("N = ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumOfDijits(int number)
+{
+    if (number == 0) return 0;
+    return (number % 10) + SumOfDijits(number / 10);
+}
+int sum = SumOfDijits(num);
+Console.WriteLine($"Сумма цифр числа = {sum}");
